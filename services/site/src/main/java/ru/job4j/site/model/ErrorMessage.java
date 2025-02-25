2dd4a20f61d2616dfebcefa8ea0f62872cb9e220
itlazykin <dzaka89@gmail.com>
2025-02-25 15:56:49 +0300

    Обработка исключений в RestTemplate [#505058 #532931]

---
+++ services/site/src/main/java/ru/job4j/site/model/ErrorMessage.java
@@ -0,0 +1,12 @@
+package ru.job4j.site.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class ErrorMessage {
+    private String message;
+}
